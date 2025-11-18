1e1ae8bf08104449555e0bf312429ac0edc09d72
hongwuwang <hongwuwang1@gmail.com>
2025-11-18 15:50:12 +0700

    11.18 app本地存储，读取

---
+++ lib/appapi/AppStorageApi.dart
@@ -0,0 +1,45 @@
+import '../Util/JsonUtil.dart';
+import 'FChatApiObj.dart';
+
+class AppStorageapi {
+  ApiObj? aobj;
+  String md;
+  String name;
+  String data="";
+
+  AppStorageapi(this.md,this.name,this.data);
+
+  save(void Function(String recdata) fchatsend){
+    aobj?.dispose();
+    if(data.isEmpty) {
+      return;
+    }
+    aobj=ApiObj(ApiName.localstorage,(value){
+      fchatsend(value);
+    });
+    aobj!.setData(toString());
+  }
+
+  read(void Function(String recdata) fchatsend){
+    aobj?.dispose();
+    aobj=ApiObj(ApiName.readstorage,(value){
+      fchatsend(value);
+    });
+    aobj!.setData(toString());
+  }
+
+  toJson(){
+    Map map={};
+    map.putIfAbsent("md", ()=> md);
+    map.putIfAbsent("name", ()=> name);
+    map.putIfAbsent("data", ()=> data);
+    return map;
+  }
+
+  @override
+  String toString(){
+    return JsonUtil.maptostr(toJson());
+  }
+
+
+}
